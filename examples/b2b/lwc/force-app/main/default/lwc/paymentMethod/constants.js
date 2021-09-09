c8f3cff1927d4f502442a3017bd2f53348fa43fd
Ashwin John <ajohn@salesforce.com>
2021-09-09 10:34:43 -0700

    feat: add in changes for 232

--- examples/b2b/lwc/force-app/main/default/lwc/paymentMethod/constants.js
+++ examples/b2b/lwc/force-app/main/default/lwc/paymentMethod/constants.js
@@ -1,11 +1,10 @@
-export const PaymentTypeEnum  = Object.freeze({
+export const PaymentTypeEnum = Object.freeze({
     PONUMBER: 'PurchaseOrderNumber',
     CARDPAYMENT: 'CardPayment'
 });
 
-export const labels  = {
+export const labels = {
     paymentMethodHeader: 'Payment Method',
-    buttonLabel: 'Submit Payment',
     purchaseOrderOptionLabel: 'Purchase Order',
     purchaseOrderEntryHeader: 'Enter PO Number',
     cardPaymentOptionLabel: 'Credit Card'
