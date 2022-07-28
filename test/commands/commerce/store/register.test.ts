bc0cec46551ad8252e82b1c7042ae70dba2c9644
unandyala <unandyala@salesforce.com>
2022-07-28 09:47:05 -0400

    fix:unit tests

--- test/commands/commerce/store/register.test.ts
+++ test/commands/commerce/store/register.test.ts
@@ -11,7 +11,6 @@
 // import { UX } from '@salesforce/command';
 // import { QueryResult } from '@mshanemc/plugin-helpers/dist/typeDefs';
 // import { expect, test } from '@salesforce/command/lib/test';
-// import * as forceOrgSoqlExports from '../../../../src/lib/utils/sfdx/forceDataSoql';
 // import { Org, OrgListResult, Result } from '../../../../src/lib/utils/jsonUtils';
 // import { StatusFileManager } from '../../../../src/lib/utils/statusFileManager';
 // // import { RegisterExtension } from '../../../../src/commands/commerce/extension/register';
@@ -20,22 +19,74 @@
 // import { getHubOrgByUsername } from '../../../../src/lib/utils/sfdx/forceOrgList';
 // import * as forceOrgListExports from '../../../../src/lib/utils/sfdx/forceOrgList';
 
-// describe('commerce:extension:register', () => {
-//     let uxStub: StubbedType<UX>;
-//     afterEach(() => {
-//         sinon.restore();
-//     });
-//     // get hub org by username
-//     it('should get hub org by username', async () => {
-//         const res = new Result<OrgListResult>();
-//         res.status = 0;
-//         res.result = new OrgListResult();
-//         res.result.nonScratchOrgs.push(new Org('a'));
-//         res.result.nonScratchOrgs.push(new Org('b'));
-//         const s = stub(forceOrgListExports, 'forceOrgList').returns(res);
-//         assert.equal(getHubOrgByUsername('a').username, 'a');
-//         s.restore();
-//     });
+import { strict as assert } from 'assert';
+import { stubInterface } from '@salesforce/ts-sinon';
+import { IConfig } from '@oclif/config';
+import { $$ } from '@salesforce/command/lib/test';
+import sinon from 'sinon';
+import { SfdxError } from '@salesforce/core';
+import { QueryResult } from '@mshanemc/plugin-helpers/dist/typeDefs';
+import { RegisterExtension } from '../../../../src/commands/commerce/extension/register';
+import * as forceOrgSoqlExports from '../../../../src/lib/utils/sfdx/forceDataSoql';
+import { Result } from '../../../../src/lib/utils/jsonUtils';
+
+describe('Test extension register function', () => {
+    const config = stubInterface<IConfig>($$.SANDBOX, {});
+    const registeredExtensionName = 'test';
+    const epn = 'testEPN';
+    const apexClass = 'testApex';
+    const orgUserName = 'testUser';
+    const QUERY_GET_APEX_CLASS = `SELECT Id FROM ApexClass WHERE Name='${apexClass}' LIMIT 1`;
+    const QUERY_GET_EPN_LIST = `SELECT Value FROM PicklistValueInfo WHERE Value='${epn}' AND EntityParticle.DurableId = 'RegisteredExternalService.ExtensionPointName' LIMIT 1`;
+
+    const forceDataSoqlStub = sinon.stub(forceOrgSoqlExports, 'forceDataSoql');
+    const registerExtension = new RegisterExtension([], config);
+
+    beforeEach(() => {
+        forceDataSoqlStub.reset();
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    it('Throws error with a invalid Apex class', async () => {
+        forceDataSoqlStub.withArgs(QUERY_GET_APEX_CLASS, orgUserName).throws(new SfdxError('Invalid Apex'));
+        assert.throws(
+            () => registerExtension.registerApex(registeredExtensionName, epn, apexClass, orgUserName),
+            SfdxError
+        );
+        assert(forceDataSoqlStub.calledWith(QUERY_GET_APEX_CLASS, orgUserName));
+    });
+    it('Throws error with a invalid EPN', async () => {
+        const qr = new Result<QueryResult>();
+        qr.result = new (class implements QueryResult {
+            public done: boolean;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            public records: Record[] = [{ Id: 'hi' }];
+            public totalSize = 1;
+        })();
+        forceDataSoqlStub.withArgs(QUERY_GET_APEX_CLASS, 'testUser').returns(qr);
+        // stub EPN query call
+        const epnQr = new Result<QueryResult>();
+        epnQr.result = new (class implements QueryResult {
+            public done: boolean;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            public records: Record[] = [];
+            public totalSize = 0;
+        })();
+        forceDataSoqlStub.withArgs(QUERY_GET_EPN_LIST).returns(epnQr);
+        assert.throws(
+            () => registerExtension.registerApex(registeredExtensionName, epn, apexClass, orgUserName),
+            SfdxError
+        );
+        assert(forceDataSoqlStub.calledWith(QUERY_GET_APEX_CLASS, orgUserName));
+        assert(forceDataSoqlStub.calledWith(QUERY_GET_EPN_LIST));
+    });
+});
+
 //     // get storeid using create command's method
 //     it('should get the storeid using getStoreId()', async () => {
 //         const sfm = new StatusFileManager('a', 'b', 'c');
