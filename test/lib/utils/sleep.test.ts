49d6577a5e0f02611388b51eae2b2786c0b78316
Joel Uong <joel.uong@salesforce.com>
2022-10-20 10:51:17 -0400

    update unit test

--- test/lib/utils/sleep.test.ts
+++ test/lib/utils/sleep.test.ts
@@ -5,14 +5,13 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { strict as assert } from 'assert';
-import { performance } from 'perf_hooks';
 import { sleep } from '../../../src/lib/utils/sleep';
 
 describe('Sleep', () => {
     it('should sleep for 10 ms', async () => {
-        const start = performance.now();
+        const start = Date.now();
         await sleep(10);
-        const end = performance.now();
+        const end = Date.now();
         const sleepTime = end - start;
         assert.equal(sleepTime >= 10 && sleepTime < 35, true);
     });
