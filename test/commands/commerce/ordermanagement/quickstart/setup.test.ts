b2f6e4372e25db53fee3d5c84fadc73a7f2b6328
unandyala <unandyala@salesforce.com>
2023-03-12 05:26:39 -0400

    SOM setup

--- test/commands/commerce/ordermanagement/quickstart/setup.test.ts
+++ test/commands/commerce/ordermanagement/quickstart/setup.test.ts
@@ -10,7 +10,7 @@ import { IConfig } from '@oclif/config';
 import { $$ } from '@salesforce/command/lib/test';
 import sinon, { stub } from 'sinon';
 import { UX } from '@salesforce/command';
-import { Logger, Org } from '@salesforce/core';
+import { Logger, Org, SfdxError } from '@salesforce/core';
 import { QueryResult } from '@mshanemc/plugin-helpers/dist/typeDefs';
 import { OrderManagementQuickstartSetup } from '../../../../../src/commands/commerce/ordermanagement/quickstart/setup';
 import * as shellExports from '../../../../../src/lib/utils/shell';
@@ -18,14 +18,13 @@ import { Result } from '../../../../../src/lib/utils/jsonUtils';
 import { EXAMPLE_DIR } from '../../../../../src/lib/utils/constants/properties';
 import { QUERY_LOCATION_TYPES } from '../../../../../src/commands/commerce/ordermanagement/quickstart/setup';
 import { SObject } from 'jsforce';
-import { Callback } from 'jsforce/connection';
 import { Query } from 'jsforce/query';
-// import * as assert from 'assert';
+import * as assert from 'assert';
 
 describe('Test order management setup command', () => {
     const config = stubInterface<IConfig>($$.SANDBOX, {});
-    const logger = sinon.createStubInstance(Logger);
-    const org = sinon.createStubInstance(Org);
+    const logger = new Logger('test');
+    const org = new Org({});
     const username = 'test@1commerce.com';
     const apiversion = '57.0';
     const orderManagementQuickstartSetup = Object.assign(new OrderManagementQuickstartSetup([], config), {
@@ -34,7 +33,7 @@ describe('Test order management setup command', () => {
         logger,
         flags: { apiversion: apiversion },
     });
-    const shellStub = stub(shellExports, 'shellJsonSfdx');
+
     const deployResult = {
         status: 0,
         name: '',
@@ -53,10 +52,11 @@ describe('Test order management setup command', () => {
     });
 
     it('successfully sets up the order management', async () => {
-        const usernameStub = org.getUsername.callsFake(() => 'test@1commerce.com');
+        const shellStub = stub(shellExports, 'shellJsonSfdx');
+        const usernameStub = stub(org, 'getUsername').returns(username);
         // @ts-ignore
-        const connectionStub = org.getConnection.returns({
-            sobject<T = object>(resource: string): SObject<T> {
+        const connectionStub = stub(org, 'getConnection').returns({
+            sobject<T = object>(): SObject<T> {
                 // @ts-ignore
                 return {
                     // @ts-ignore
@@ -64,10 +64,7 @@ describe('Test order management setup command', () => {
                         return Promise.resolve({ success: true });
                     },
                     // @ts-ignore
-                    select(
-                        fields?: { [P in keyof T]: boolean } | Array<keyof T> | keyof T,
-                        callback?: Callback<Array<Partial<T>>>
-                    ): Query<Array<Partial<T>>> {
+                    select(): Query<Array<Partial<T>>> {
                         // @ts-ignore
                         return Promise.resolve([{ locationType: 'warehouse' }]);
                     },
@@ -98,9 +95,10 @@ describe('Test order management setup command', () => {
 
         [shellStub, usernameStub, connectionStub].forEach((stub) => stub.restore());
     });
-    /*
+
     it('throws error when flows deployment fails', async () => {
-        const usernameStub = org.getUsername.returns(username);
+        const shellStub = stub(shellExports, 'shellJsonSfdx');
+        const usernameStub = stub(org, 'getUsername').returns(username);
         const deployCommand = `sfdx force:mdapi:deploy -u "${username}" -d ${EXAMPLE_DIR}/som/ -w 1 --apiversion=${apiversion}`;
         shellStub.withArgs(deployCommand).throws(new SfdxError('Test'));
 
@@ -108,11 +106,12 @@ describe('Test order management setup command', () => {
         sinon.assert.calledOnce(shellStub);
         sinon.assert.calledWith(shellStub, deployCommand);
 
-        [shellStub, usernameStub].forEach((stub) => stub.reset());
+        [shellStub, usernameStub].forEach((stub) => stub.restore());
     });
 
     it('throws error when actions deployment fails', async () => {
-        const usernameStub = org.getUsername.returns(username);
+        const shellStub = stub(shellExports, 'shellJsonSfdx');
+        const usernameStub = stub(org, 'getUsername').returns(username);
         const deployCommand = `sfdx force:mdapi:deploy -u "${username}" -d ${EXAMPLE_DIR}/som/ -w 1 --apiversion=${apiversion}`;
         shellStub.withArgs(deployCommand).returns(deployResult);
         const deployActionsCommand = `sfdx force:mdapi:deploy -u "${username}" -d ${EXAMPLE_DIR}/som/actions/ -w 1 --apiversion=${apiversion}`;
@@ -121,14 +120,15 @@ describe('Test order management setup command', () => {
         sinon.assert.calledTwice(shellStub);
         sinon.assert.calledWith(shellStub, deployCommand);
 
-        [shellStub, usernameStub].forEach((stub) => stub.reset());
+        [shellStub, usernameStub].forEach((stub) => stub.restore());
     });
 
     it('throws error when location api fails', async () => {
-        const usernameStub = org.getUsername.returns(username);
+        const shellStub = stub(shellExports, 'shellJsonSfdx');
+        const usernameStub = stub(org, 'getUsername').returns(username);
         // @ts-ignore
-        const connectionStub = org.getConnection.returns({
-            sobject<T = object>(resource: string): SObject<T> {
+        const connectionStub = stub(org, 'getConnection').returns({
+            sobject<T = object>(): SObject<T> {
                 // @ts-ignore
                 return {
                     // @ts-ignore
@@ -136,10 +136,7 @@ describe('Test order management setup command', () => {
                         return Promise.resolve({ success: false });
                     },
                     // @ts-ignore
-                    select(
-                        fields?: { [P in keyof T]: boolean } | Array<keyof T> | keyof T,
-                        callback?: Callback<Array<Partial<T>>>
-                    ): Query<Array<Partial<T>>> {
+                    select(): Query<Array<Partial<T>>> {
                         // @ts-ignore
                         return Promise.resolve([{ locationType: 'warehouse' }]);
                     },
@@ -180,6 +177,6 @@ describe('Test order management setup command', () => {
         sinon.assert.calledWith(shellStub, deployCommand);
         sinon.assert.calledWith(shellStub, deployActionsCommand);
 
-        [shellStub, usernameStub, connectionStub].forEach((stub) => stub.reset());
-    });*/
+        [shellStub, usernameStub, connectionStub].forEach((stub) => stub.restore());
+    });
 });
