87c70a50428323e2b7eb2be66bec971cf1d07e64
tarcan-gul <tarcan.gul@salesforce.com>
2023-11-14 16:16:54 -0500

    Edited prepare script, running del on windows

---
+++ src/scripts/prepare.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as os from 'os';
+import { spawn } from 'child_process';
+
+let command = '';
+
+switch (os.platform()) {
+    case 'win32':
+        command = 'del -f .oclif.manifest.json && yarn run prepack && husky install';
+        break;
+    default:
+        command = 'rm -f .oclif.manifest.json && yarn run prepack && husky install';
+        break;
+}
+
+const child = spawn(command, { shell: true });
+
+child.stderr.on('data', (data) => {
+    // eslint-disable-next-line no-console
+    console.error(String(data));
+});
+
+child.stdout.on('data', (data) => {
+    // eslint-disable-next-line no-console
+    console.log(String(data));
+});
