b3c5abfdec80452e6e9ed33bd5bda0eccf6d52e7
tarcan-gul <tarcan.gul@salesforce.com>
2023-12-04 15:22:12 -0500

    Edit prepare command, run powershell instead of cmd

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -8,17 +8,20 @@ import * as os from 'os';
 import { spawn } from 'child_process';
 
 let command = '';
+let shellOptions = {};
 
 switch (os.platform()) {
     case 'win32':
-        command = 'del -f .oclif.manifest.json && yarn run prepack && husky install';
+        command = 'Remove-Item -Force .oclif.manifest.json; yarn run prepack; husky install';
+        shellOptions = { shell : 'powershell.exe' };
         break;
     default:
         command = 'rm -f .oclif.manifest.json && yarn run prepack && husky install';
+        shellOptions = { shell : '/bin/sh' };
         break;
 }
 
-const child = spawn(command, { shell: true });
+const child = spawn(command, shellOptions);
 
 child.stderr.on('data', (data) => {
     // eslint-disable-next-line no-console
