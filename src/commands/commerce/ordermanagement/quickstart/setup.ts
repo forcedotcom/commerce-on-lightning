5fde56610f71c3c0ca46c7e00791c35ba41b17cc
Bharath Kumar Adhikari <b.adhikari@salesforce.com>
2024-05-27 12:54:11 +0530

    updated deploy parameter

--- src/commands/commerce/ordermanagement/quickstart/setup.ts
+++ src/commands/commerce/ordermanagement/quickstart/setup.ts
@@ -52,7 +52,7 @@ export class OrderManagementQuickstartSetup extends SfdxCommand {
         this.ux.log(msgs.getMessage('quickstart.setup.deployFlowsToOrg'));
         let deployResult = shellJsonSfdx(
             appendCommonFlags(
-                `sf project deploy start --metadata-dir --target-org "${this.org.getUsername()}" --deploydir ${EXAMPLE_DIR}/som/ --wait 1`,
+                `sf project deploy start --target-org "${this.org.getUsername()}" --metadata-dir ${EXAMPLE_DIR}/som/ --wait 1`,
                 this.flags,
                 this.logger
             )
